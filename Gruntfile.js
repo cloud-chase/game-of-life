4c513fe4ae3cb52a15133547183ffa4d4678cfa0
lee-chase <lee.chase@uk.ibm.com>
2015-09-10 18:37:37 +0100

    grunt me a build first

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,15 @@
 module.exports = function(grunt) {
-  
+
   var //js_dependencies = 'js/**/*.js',
       //js_to_minify = [ 'js/app/*.js', 'js/*.js' ],
       //js_minified = 'js/app.min.js',
-      
+
       sass_dependencies = 'css/**/*.scss',
       sass_to_compile = 'css/GoL.scss',
       sass_compiled = 'css/GoL.css';
 
   grunt.initConfig({
-    
+
     // make our package meta-data available as fields in 'pkg'
     pkg: grunt.file.readJSON('package.json'),
 
@@ -19,24 +19,24 @@ module.exports = function(grunt) {
         dest: sass_compiled
       }
     },
-    
+
     /*uglify: {
       options: {
         banner: '/' + '*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> *' + '/\n'
       },
-        
+
       custom: {
         src: js_to_minify,
         dest: js_minified
       }
     },*/
-    
+
     watch: {
       sass_build: {
         files: sass_dependencies,
         tasks: 'sass:build'
       },
-      
+
       /*js_build: {
         files: js_dependencies,
         tasks: 'uglify:custom'
@@ -53,5 +53,5 @@ module.exports = function(grunt) {
   grunt.registerTask('build', [ 'sass:build'/*, 'uglify'*/ ]);
   grunt.registerTask('test', [ ]);
 
-  grunt.registerTask('default', [ 'watch' ]);
+  grunt.registerTask('default', [ 'build', 'watch' ]);
 };
