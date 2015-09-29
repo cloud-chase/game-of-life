9f2fc91925cfc31c6527943e0a8224098813ddbf
Dave Clark <dave.clark@uk.ibm.com>
2015-09-29 22:54:56 +0100

    Manage property bag lifecycle

--- js/app/sparse-2d-array.js
+++ js/app/sparse-2d-array.js
@@ -1,34 +1,31 @@
 
 define(function() {
-
-  var that = {};
-  
-  that.new = function() {
-    var self = [];
+  return {
+    new: function() {
+      var that = [];
     
-    Object.defineProperty(self, 'set', { writable: false, value: function(a, b, value) {
-      var slice = self[a];
-      if (!slice) {
-        slice = self[a] = {};
-        // include a non-enumerable 'count' property to keep track of entries
-        Object.defineProperty(slice, 'count', { writable: true, value: 1 });
-      } else if (!slice[b]) {
-        slice.count++;
-      }
-      slice[b] = value;
-    }});
-
-    Object.defineProperty(self, 'delete', { writable: false, value: function(a, b) {
-      if (self[a]) {
-        delete self[a][b];
-        if (--self[a].count === 0) {
-          delete self[a];
+      Object.defineProperty(that, 'set', { writable: false, value: function(a, b, value) {
+        var slice = that[a];
+        if (!slice) {
+          slice = that[a] = {};
+          // include a non-enumerable 'count' property to keep track of entries
+          Object.defineProperty(slice, 'count', { writable: true, value: 1 });
+        } else if (!slice[b]) {
+          slice.count++;
         }
-      }
-    }});
-    
-    return self;
+        slice[b] = value;
+      }});
+
+      Object.defineProperty(that, 'delete', { writable: false, value: function(a, b) {
+        if (that[a]) {
+          delete that[a][b];
+          if (--that[a].count === 0) {
+            delete that[a];
+          }
+        }
+      }});
+
+      return that;
+    }
   };
-  
-  return that;
 });
